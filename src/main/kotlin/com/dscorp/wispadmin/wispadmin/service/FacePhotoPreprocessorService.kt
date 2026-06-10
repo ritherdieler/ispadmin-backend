430aae39ced48522a945c8f00b6c0b7e566bbaad
EDWIN <benavidesedwin170@gmail.com>
2026-06-10 15:29:07 -0500

    nueva actualizacion de facial

--- src/main/kotlin/com/dscorp/wispadmin/wispadmin/service/FacePhotoPreprocessorService.kt
+++ src/main/kotlin/com/dscorp/wispadmin/wispadmin/service/FacePhotoPreprocessorService.kt
@@ -30,7 +30,7 @@ import kotlin.math.roundToInt
  */
 @Service
 class FacePhotoPreprocessorService(
-    @Value("\${face.login.djl-detector-model-path:C:/ispadmin/models/ultranet.zip}")
+    @Value("\${face.login.djl-detector-model-path:src/main/kotlin/com/dscorp/wispadmin/wispadmin/util/models/ultranet.zip}")
     private val detectorModelPath: String,
     @Value("\${face.login.djl-detector-model-name:ultranet}")
     private val detectorModelName: String
